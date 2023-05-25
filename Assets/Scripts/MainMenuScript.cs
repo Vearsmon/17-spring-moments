0c3ed4f1d691975c575d055a76f281aefaabcef3
rispele <50751327+Rispele@users.noreply.github.com>
2023-05-25 10:30:03 +0500

    Add save-load feature

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Model;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,11 +8,15 @@ public class MainMenuScript : MonoBehaviour
 {
     public void StartGame()
     {
+        Core.TryLoad();
         SceneManager.LoadScene("House");
     }
 
     public void QuitGame()
     {
+        if (!Core.TrySave())
+            return;
+
         Debug.Log("Выход из игры завершен");
         Application.Quit();
     }
--- Assets/Scripts/Model/Core.cs
+++ Assets/Scripts/Model/Core.cs
@@ -1,10 +1,65 @@
-﻿namespace Model
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+// Для того чтобы record работал корректно
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit {}
+}
+
+namespace Model
 {
     public static class Core
     {
-        public static readonly House HouseState = new();
-        public static readonly Balcony BalconyState = new();
+        public static House HouseState { get; private set; } = new();
+        public static Balcony BalconyState { get; private set; }= new();
 
-        public static readonly PlayerState PlayerState = new();
+        public static PlayerState PlayerState { get; private set; }= new();
+
+        public static bool TrySave()
+        {
+            try
+            {
+                var serialized = JsonConvert.SerializeObject(new CoreData(HouseState, BalconyState, PlayerState));
+                
+                var sw = new StreamWriter("save", true);
+                sw.Write(serialized);
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to save");
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool TryLoad()
+        {
+            try
+            {
+                var sw = new StreamReader("save");
+                var serialized = sw.ReadToEnd();
+                sw.Close();
+
+                var coreData = JsonConvert.DeserializeObject<CoreData>(serialized);
+                HouseState = coreData!.House;
+                BalconyState = coreData!.Balcony;
+                PlayerState = coreData!.PlayerState;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to load");
+                Console.WriteLine(e.Message);
+                return false;
+            }
+
+            return true;
+        }
+        
+        private record CoreData(House House, Balcony Balcony, PlayerState PlayerState);
     }
 }
